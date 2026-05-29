7187ee39f30b648d49f59fdda7afb59103b9af63
Diantech PH <118250070+TarsalBlade@users.noreply.github.com>
2026-05-29 16:23:15 +0800

    Update dashboard pages index file with new separate page exports

--- src/pages/dashboard/index.ts
+++ src/pages/dashboard/index.ts
@@ -1,3 +1,6 @@
 export { SuperAdminDashboard } from './SuperAdminDashboard';
 export { LendingAdminDashboard } from './LendingAdminDashboard';
 export { BorrowerDashboard } from './BorrowerDashboard';
+export { MyApplicationsPage } from './MyApplicationsPage';
+export { MyLoansPage } from './MyLoansPage';
+export { MyProfilePage } from './MyProfilePage';
